1d33828b92ff9407129f83caa12574ee6b0bac0f
Fabio Piazzarollo <fbpzrl@gmail.com>
2022-01-28 21:17:11 -0300

    H2 database

--- Bootcamp Spring/dscatalog/backend/src/main/java/com/devsuperior/dscatalog/resources/CategoryResource.java
+++ Bootcamp Spring/dscatalog/backend/src/main/java/com/devsuperior/dscatalog/resources/CategoryResource.java
@@ -1,19 +1,23 @@
 package com.devsuperior.dscatalog.resources;
 
-import java.util.ArrayList;
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.devsuperior.dscatalog.entities.Category;
+import com.devsuperior.dscatalog.services.CategoryService;
 
 @RestController //Defines that the class is a Rest Controller
 @RequestMapping(value = "/categories") //Defines the resource's Rest route
 public class CategoryResource { //Resource implements the Rest Controller
 
+	@Autowired //Automatically inject dependency
+	private CategoryService service;
+	
 	/*
 	 * 	Endpoint (or Web Service) is a route that gives some answer.
 		Each resource can have multiple endpoints.
@@ -24,14 +28,9 @@ public class CategoryResource { //Resource implements the Rest Controller
 		- etc
 	 */
 	
-	
-	
 	@GetMapping //Defines that the method is a EndPoint
 	public ResponseEntity<List<Category>> findAll() { //ResponseEntity is a Spring object that will encapsulate an Http response
-		
-		List<Category> list = new ArrayList<>();
-		list.add(new Category(1L, "Books"));
-		list.add(new Category(2L, "Electronics"));
+		List<Category> list = service.findAll();
 		
 		return ResponseEntity.ok().body(list);
 	}
