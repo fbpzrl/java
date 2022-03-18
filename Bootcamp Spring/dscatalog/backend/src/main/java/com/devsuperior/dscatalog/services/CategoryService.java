c9af3b20ca8bd06e3b4d058730a9439551946a03
Fabio Piazzarollo <fbpzrl@gmail.com>
2022-03-18 15:06:34 -0300

    POST new Category

--- Bootcamp Spring/dscatalog/backend/src/main/java/com/devsuperior/dscatalog/services/CategoryService.java
+++ Bootcamp Spring/dscatalog/backend/src/main/java/com/devsuperior/dscatalog/services/CategoryService.java
@@ -42,4 +42,12 @@ public class CategoryService {
 		Category category = obj.orElseThrow(() -> new EntityNotFoundException("Entity not found"));
 		return new CategoryDTO(category);
 	}
+
+	@Transactional
+	public CategoryDTO insert(CategoryDTO dto) {
+		Category category = new Category();
+		category.setName(dto.getName());
+		category = repository.save(category);
+		return new CategoryDTO(category);
+	}
 }
