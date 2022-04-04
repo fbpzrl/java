e0e791dfa6afb4cda587212e68a5dd14498d604b
Fabio Piazzarollo <fbpzrl@gmail.com>
2022-04-04 11:14:09 -0300

    Exercícios com JUnit5

--- Bootcamp Spring/aula-junit/src/tests/entities/AccountTests.java
+++ Bootcamp Spring/aula-junit/src/tests/entities/AccountTests.java
@@ -11,35 +11,35 @@ public class AccountTests {
 	@Test
 	public void depositShouldIncreaseBalanceAndDiscountFeeWhenPositiveAmount() {
 		double amount = 200.0;
-		double expectedValue = 196.0; //ASSERT (EXPECTED RESULT)
+		double expectedValue = 196.0; 
 		Account acc = AccountFactory.createEmptyAccount(); //ARRANGE (INSTANTIATE NECESSARY OBJECTS)
 		
 		acc.deposit(amount); //ACTION (EXECUTE NECESSARY ACTIONS)
 		
-		Assertions.assertEquals(expectedValue, acc.getBalance());
+		Assertions.assertEquals(expectedValue, acc.getBalance()); //ASSERT (EXPECTED RESULT)
 	}
 	
 	@Test
 	public void depositShouldDoNothingWhenNegativeAmount() {
-		double expectedValue = 100.0; //ASSERT
+		double expectedValue = 100.0; 
 		Account acc = AccountFactory.createAccount(expectedValue); //ARRANGE
 		double amount = -200.0;
 		
 		acc.deposit(amount); //ACTION
 		
-		Assertions.assertEquals(expectedValue, acc.getBalance());
+		Assertions.assertEquals(expectedValue, acc.getBalance()); //ASSERT
 	}
 	
 	@Test
 	public void fullWithdrawShouldClearBalanceAndReturnFullBalance() {
-		double expectedValue = 0.0; //ASSERT
+		double expectedValue = 0.0; 
 		double initialBalance = 800.0;
 		Account acc = AccountFactory.createAccount(initialBalance); //ARRANGE
 		
 		double result = acc.fullWithdraw(); //ACTION
 		
 		Assertions.assertTrue(expectedValue == acc.getBalance());
-		Assertions.assertTrue(result == initialBalance);
+		Assertions.assertTrue(result == initialBalance); //ASSERT
 	}
 	
 	@Test
