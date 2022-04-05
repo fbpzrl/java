2cc362f09d96646324d14741d6956d9d102ef557
Fabio Piazzarollo <fbpzrl@gmail.com>
2022-04-05 09:22:34 -0300

    Refactor pageable

--- Bootcamp Spring/dscatalog/backend/src/main/java/com/devsuperior/dscatalog/resources/CategoryResource.java
+++ Bootcamp Spring/dscatalog/backend/src/main/java/com/devsuperior/dscatalog/resources/CategoryResource.java
@@ -1,12 +1,10 @@
 package com.devsuperior.dscatalog.resources;
 
 import java.net.URI;
-import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
-import org.springframework.data.domain.PageRequest;
-import org.springframework.data.domain.Sort.Direction;
+import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -15,7 +13,6 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
@@ -40,17 +37,9 @@ public class CategoryResource { //Resource implements the Rest Controller
 	 */
 	
 	@GetMapping //Defines that the method is a EndPoint
-	public ResponseEntity<Page<CategoryDTO>> findAll( // ResponseEntity is a Spring object that will encapsulate an Http response
-			@RequestParam(value = "page", defaultValue = "0") Integer page, // @RequestParam is used when the parameter is optional
-			@RequestParam(value = "linesPerPage", defaultValue = "12") Integer linesPerPage,
-			@RequestParam(value = "direction", defaultValue = "ASC") String direction,
-			@RequestParam(value = "orderBy", defaultValue = "name") String orderBy
-			
-			) { 
+	public ResponseEntity<Page<CategoryDTO>> findAll(Pageable pageable) { // ResponseEntity is a Spring object that will encapsulate an Http response 
 		
-		PageRequest pageRequest = PageRequest.of(page, linesPerPage, Direction.valueOf(direction), orderBy);
-		
-		Page<CategoryDTO> list = service.findAllPaged(pageRequest);
+		Page<CategoryDTO> list = service.findAllPaged(pageable);
 		return ResponseEntity.ok().body(list);
 	}
 	
