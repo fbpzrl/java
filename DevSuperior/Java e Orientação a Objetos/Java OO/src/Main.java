4367376ef6934bc5644f8a91e3bb2b0ce9808392
Fabio Piazzarollo <fbpzrl@gmail.com>
2023-04-06 10:32:31 -0300

    While Loop

--- DevSuperior/Java e Orientação a Objetos/Java OO/src/Main.java
+++ DevSuperior/Java e Orientação a Objetos/Java OO/src/Main.java
@@ -1,8 +1,10 @@
 import _01_Introducao.OlaMundo;
+import _04_Estruturas_Repetitivas.Loop;
 
 public class Main {
     public static void main(String[] args) {
 
-        OlaMundo.dizerOla();
+        //OlaMundo.dizerOla();
+        Loop.whileLoop();
     }
 }
---
+++ DevSuperior/Java e Orientação a Objetos/Java OO/src/_04_Estruturas_Repetitivas/Loop.java
@@ -0,0 +1,25 @@
+package _04_Estruturas_Repetitivas;
+
+import java.util.Scanner;
+public class Loop {
+
+    public static void whileLoop() {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Informe um número maior que zero: ");
+        int numero = sc.nextInt();
+
+        while (numero != 0) { //usado quando não se sabe previamente a quantidade de repetições
+
+            System.out.println("O número digitado foi: " + numero);
+            System.out.println();
+            System.out.print("Informe outro número: ");
+            numero = sc.nextInt();
+
+            if (numero == 0) {
+                System.out.println("Você digitou 0. FIM DO PROGRAMA!");
+            }
+        }
+    }
+}
